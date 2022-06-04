fb06db40fb2fc7528cd3ad995a09879953e83cc8
philipdesmedt <philip.desmedt@gmail.com>
2022-06-04 10:35:24 +0200

    wip

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,14 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const axios = require('axios');
 const url = 'https://stacks-node-api.mainnet.stacks.co/v2/info';
 const fs = require('fs');
-const enabledJobIds = [];
+const enabledJobIds = [1, 2];
+
+// scan mempool:
+// https://stacks-node-api.stacks.co/extended/v1/tx/mempool?address=SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-job-registry-v1-1&unanchored=true
+
+const runJob = async (jobId) => {
+  
+};
 
 const exec = async () => {
   response = await axios(url, { credentials: 'omit' });
@@ -13,6 +20,7 @@ const exec = async () => {
 
     if (lastBlock > lastBlockExecution) {
       console.log('Last executed on block', lastBlockExecution, '... running automation bot');
+
       fs.writeFile('lastBlockExecution', lastBlock.toString(), 'utf8', function (x, y) { console.log(x,y); });
     }
   });
