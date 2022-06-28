da26ded21b00b97c23ed5ed17854ba2d4670c047
Niel <hello@nieldeckx.be>
2022-06-28 19:57:55 +0200

    tests: added helper models for job registry

--- clarity/tests/arkadiko-job-registry-v1-1_test.ts
+++ clarity/tests/arkadiko-job-registry-v1-1_test.ts
@@ -6,72 +6,67 @@ import {
   types,
 } from "https://deno.land/x/clarinet@v0.31.0/index.ts";
 
+import { 
+  JobRegistry,
+} from './models/arkadiko-job-registry-helpers.ts';
+
 import * as Utils from './models/arkadiko-tests-utils.ts';
+
+Clarinet.test({name: "job registry: register job",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer")!;
+    let wallet_1 = accounts.get("wallet_1")!;
+
+    let jobRegistry = new JobRegistry(chain, deployer);
+
+    let result = jobRegistry.registerJob(wallet_1, "job-diko-liquidation-pool", 0.1, "arkadiko-job-cost-calculation-v1-1");
+    result.expectOk().expectBool(true);
+
+    let call = await jobRegistry.getJobById(1);
+    call.result.expectTuple()['enabled'].expectBool(true);
+    call.result.expectTuple()['owner'].expectPrincipal('ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5');
+    call.result.expectTuple()['contract'].expectPrincipal(Utils.qualifiedName("job-diko-liquidation-pool"));
+    call.result.expectTuple()['cost'].expectUintWithDecimals(10);
+    call.result.expectTuple()['fee'].expectUintWithDecimals(0.1);
+    call.result.expectTuple()['last-executed'].expectUint(0);
+
+    result = jobRegistry.runJob(deployer, 1, "job-diko-liquidation-pool", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(false); // execution not required
+  }
+});
 
 Clarinet.test({name: "job registry: register and run job",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
     let wallet_1 = accounts.get("wallet_1")!;
 
-    let block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "register-job", [
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool")),
-        types.uint(100000),
-        types.principal(Utils.qualifiedName("arkadiko-job-cost-calculation-v1-1")),
-      ], wallet_1.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(true);
+    let jobRegistry = new JobRegistry(chain, deployer);
 
-    let call = chain.callReadOnlyFn("arkadiko-job-registry-v1-1", "get-job-by-id", [types.uint(1)], deployer.address);
-    call.result.expectTuple()['owner'].expectPrincipal('ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5');
+    let result = jobRegistry.creditAccount(wallet_1, 1000, 1000);
+    result.expectOk().expectBool(true);
 
-    block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
-        types.uint(1),
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool")),
-        types.principal(Utils.qualifiedName("arkadiko-job-executor-v1-1")),
-      ], deployer.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(false); // execution not required
+    result = jobRegistry.registerJob(wallet_1, "job-diko-liquidation-pool", 0.1, "arkadiko-job-cost-calculation-v1-1");
+    result.expectOk().expectBool(true);
 
-    block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "register-job", [
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool-2")),
-        types.uint(100000),
-        types.principal(Utils.qualifiedName("arkadiko-job-cost-calculation-v1-1")),
-      ], wallet_1.address),
-      Tx.contractCall("arkadiko-job-registry-v1-1", "credit-account", [
-        types.principal(wallet_1.address),
-        types.uint(1000000000),
-        types.uint(1000000000),
-      ], wallet_1.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(true);
+    result = jobRegistry.runJob(deployer, 1, "job-diko-liquidation-pool", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(false); // execution not required
+    
+    result = jobRegistry.registerJob(wallet_1, "job-diko-liquidation-pool-2", 0.1, "arkadiko-job-cost-calculation-v1-1");
+    result.expectOk().expectBool(true);
 
-    block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
-        types.uint(2),
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool-2")),
-        types.principal(Utils.qualifiedName("arkadiko-job-executor-v1-1")),
-      ], deployer.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(true);
+    result = jobRegistry.runJob(deployer, 2, "job-diko-liquidation-pool-2", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(true);
   }
 });
 
 Clarinet.test({name: "job registry: try running unregistered job",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
-    let wallet_1 = accounts.get("wallet_1")!;
 
-    let block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
-        types.uint(1),
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool")),
-        types.principal(Utils.qualifiedName("arkadiko-job-executor-v1-1")),
-      ], wallet_1.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(false);
+    let jobRegistry = new JobRegistry(chain, deployer);
+
+    let result = jobRegistry.runJob(deployer, 1, "job-diko-liquidation-pool", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(false); // execution not required
   }
 });
 
@@ -80,31 +75,19 @@ Clarinet.test({name: "job registry: do not run disabled job",
     let deployer = accounts.get("deployer")!;
     let wallet_1 = accounts.get("wallet_1")!;
 
-    let block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "register-job", [
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool-2")),
-        types.uint(100000),
-        types.principal(Utils.qualifiedName("arkadiko-job-cost-calculation-v1-1")),
-      ], wallet_1.address),
-      Tx.contractCall("arkadiko-job-registry-v1-1", "credit-account", [
-        types.principal(wallet_1.address),
-        types.uint(1000000000),
-        types.uint(1000000000),
-      ], wallet_1.address),
-      Tx.contractCall("arkadiko-job-registry-v1-1", "toggle-job-enabled", [
-        types.uint(1)
-      ], wallet_1.address)
-    ]);
-    block.receipts[0].result.expectOk().expectBool(true);
+    let jobRegistry = new JobRegistry(chain, deployer);
 
-    block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
-        types.uint(1),
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool-2")),
-        types.principal(Utils.qualifiedName("arkadiko-job-executor-v1-1")),
-      ], deployer.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(false);
+    let result = jobRegistry.registerJob(wallet_1, "job-diko-liquidation-pool-2", 0.1, "arkadiko-job-cost-calculation-v1-1");
+    result.expectOk().expectBool(true);
+
+    result = jobRegistry.creditAccount(wallet_1, 1000, 1000);
+    result.expectOk().expectBool(true);
+
+    result = jobRegistry.toggleJobEnabled(wallet_1, 1);
+    result.expectOk().expectBool(true);
+
+    result = jobRegistry.runJob(deployer, 1, "job-diko-liquidation-pool-2", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(false);
   }
 });
 
@@ -113,13 +96,10 @@ Clarinet.test({name: "job registry: try stealing tokens",
     let deployer = accounts.get("deployer")!;
     let wallet_1 = accounts.get("wallet_1")!;
 
-    let block = chain.mineBlock([
-      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
-        types.uint(1),
-        types.principal(Utils.qualifiedName("job-diko-liquidation-pool")),
-        types.principal(Utils.qualifiedName("arkadiko-job-executor-v1-1")),
-      ], wallet_1.address),
-    ]);
-    block.receipts[0].result.expectOk().expectBool(false);
+    let jobRegistry = new JobRegistry(chain, deployer);
+
+    // TODO: test is wrong
+    let result = jobRegistry.runJob(wallet_1, 1, "job-diko-liquidation-pool", "arkadiko-job-executor-v1-1");
+    result.expectOk().expectBool(false);
   }
 });
---
+++ clarity/tests/models/arkadiko-job-registry-helpers.ts
@@ -0,0 +1,74 @@
+import {
+  Account,
+  Chain,
+  Clarinet,
+  Tx,
+  types,
+} from "https://deno.land/x/clarinet/index.ts";
+
+import * as Utils from './arkadiko-tests-utils.ts';
+
+// ---------------------------------------------------------
+// Job  Registry
+// ---------------------------------------------------------
+
+class JobRegistry {
+  chain: Chain;
+  deployer: Account;
+
+  constructor(chain: Chain, deployer: Account) {
+    this.chain = chain;
+    this.deployer = deployer;
+  }
+
+  getJobById(jobId: number) {
+    return this.chain.callReadOnlyFn("arkadiko-job-registry-v1-1", "get-job-by-id", [
+      types.uint(jobId),
+    ], this.deployer.address);
+  }
+
+
+  registerJob(sender: Account, contract: string, fee: number, costCalculator: string) {
+    let block = this.chain.mineBlock([
+      Tx.contractCall("arkadiko-job-registry-v1-1", "register-job", [
+        types.principal(Utils.qualifiedName(contract)),
+        types.uint(fee * 1000000),
+        types.principal(Utils.qualifiedName(costCalculator)),
+      ], sender.address),
+    ]);
+    return block.receipts[0].result;
+  }
+
+  runJob(sender: Account, jobId: number, contract: string, jobExecutor: string) {
+    let block = this.chain.mineBlock([
+      Tx.contractCall("arkadiko-job-registry-v1-1", "run-job", [
+        types.uint(jobId),
+        types.principal(Utils.qualifiedName(contract)),
+        types.principal(Utils.qualifiedName(jobExecutor)),
+      ], sender.address),
+    ]);
+    return block.receipts[0].result;
+  }
+
+  creditAccount(sender: Account, diko: number, stx: number) {
+    let block = this.chain.mineBlock([
+      Tx.contractCall("arkadiko-job-registry-v1-1", "credit-account", [
+        types.principal(sender.address),
+        types.uint(diko * 1000000),
+        types.uint(stx * 1000000),
+      ], sender.address),
+    ]);
+    return block.receipts[0].result;
+  }
+  
+  toggleJobEnabled(sender: Account, jobId: number) {
+    let block = this.chain.mineBlock([
+      Tx.contractCall("arkadiko-job-registry-v1-1", "toggle-job-enabled", [
+        types.uint(jobId),
+      ], sender.address),
+    ]);
+    return block.receipts[0].result;
+  }
+
+}
+export { JobRegistry };
