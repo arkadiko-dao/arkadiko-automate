da532d14309e2185c45e71131e12bdde7d9933da
Niel <hello@nieldeckx.be>
2022-07-08 16:08:16 +0200

    fix register-job script

--- scripts/register-job.js
+++ scripts/register-job.js
@@ -1,14 +1,9 @@
 require('dotenv').config();
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const axios = require('axios');
-const url = 'https://stacks-node-api.mainnet.stacks.co/v2/info';
-const fs = require('fs');
 const tx = require('@stacks/transactions');
 const utils = require('./utils');
 const network = utils.resolveNetwork();
 const BN = require('bn.js');
-const stacking = require('@stacks/stacking');
-const c32 = require('c32check');
 
 const registerJob = async () => {
   const txOptions = {
@@ -17,11 +12,12 @@ const registerJob = async () => {
     functionName: "register-job",
     functionArgs: [
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'job-diko-liquidation-pool'),
-      tx.uintCV(10000),
+      tx.uintCV(1000),
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-job-cost-calculation-v1-1'),
     ],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
+    fee: new BN(10000, 1),
     network
   };
 
